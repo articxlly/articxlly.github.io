aab54e615e472296e7b0ddf9ab515dd5f533dcdb
articxlly <articxlly@gmail.com>
2026-06-10 22:31:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ window.addEventListener('scroll', () => {
     sections.forEach(section => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if (scrollY >= (sectionTop - 200)) {
+        if (window.scrollY >= (sectionTop - 200)) {
             current = section.getAttribute('id');
         }
     });
